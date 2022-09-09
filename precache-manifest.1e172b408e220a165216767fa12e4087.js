ce1118667012e9a1b6ce5086fdc256d8aa5da89f
freesaf <range1@gmx.com>
2022-09-09 11:33:24 +0100

    Deploy React app to GitHub Pages

---
+++ precache-manifest.1e172b408e220a165216767fa12e4087.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac2d9c70e06fafe11c0c5444f9061df",
+    "url": "/zamma/index.html"
+  },
+  {
+    "revision": "243ffc38f8b8dae5445f",
+    "url": "/zamma/static/css/main.c66934be.chunk.css"
+  },
+  {
+    "revision": "0186e76232cdfc2f11af",
+    "url": "/zamma/static/js/2.b0b8a37a.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/zamma/static/js/2.b0b8a37a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "243ffc38f8b8dae5445f",
+    "url": "/zamma/static/js/main.a0016334.chunk.js"
+  },
+  {
+    "revision": "1b89a36a30fdc461a70b",
+    "url": "/zamma/static/js/runtime-main.7dbf24e7.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/zamma/static/media/logo.ee7cd8ed.svg"
+  }
+]);
