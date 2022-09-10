7a9133ad349e0da9a6ea21761de739838d489c63
freesaf <range1@gmx.com>
2022-09-10 08:35:44 +0100

    Deploy React app to GitHub Pages

---
+++ precache-manifest.f7b2ad0beed90c7283ce95f998a55784.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991e690a886dd824054071454d6e3496",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "8c0d76bbef3510712979",
+    "url": "/home/static/css/main.c66934be.chunk.css"
+  },
+  {
+    "revision": "0186e76232cdfc2f11af",
+    "url": "/home/static/js/2.b0b8a37a.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/home/static/js/2.b0b8a37a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c0d76bbef3510712979",
+    "url": "/home/static/js/main.11946883.chunk.js"
+  },
+  {
+    "revision": "de97020a4b3a3732384c",
+    "url": "/home/static/js/runtime-main.e1b53e6d.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/home/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/zamma/precache-manifest.5be661fce1001d111f1232b6b9617a01.js"
+  "/home/precache-manifest.f7b2ad0beed90c7283ce95f998a55784.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zamma/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
